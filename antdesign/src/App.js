9165b151e5d231f92d2361785c5a03cbaa56f98c
JordiOrtega <jordi.ortega@gmail.com>
2018-02-27 09:46:04 +0100

    Fixed nav.

--- antdesign/src/App.js
+++ antdesign/src/App.js
@@ -5,37 +5,38 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 class App extends Component {
-  render() {
-    return (
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Inicio</Breadcrumb.Item>
-            <Breadcrumb.Item>Projectos</Breadcrumb.Item>
-            <Breadcrumb.Item>CV</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2016 Created by Ant UED
-      </Footer>
-      </Layout>
-      
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Header style={{ position: 'fixed', width: '100%' }}>
+                    <div className="logo">
+                        <img src={logo} style={{ height: '80%', float: 'left' }} alt="logo" /> 
+                    </div>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©2016 Created by Ant UED
+            </Footer>
+            </Layout>
+        );
+    }
 }
 
 export default App;
